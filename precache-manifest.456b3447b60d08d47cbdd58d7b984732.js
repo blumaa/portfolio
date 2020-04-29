8cec4b807573e776b7bf3247ff9dcd4325762933
blumaa <blumaa@gmail.com>
2020-04-30 00:04:11 +0200

    build

---
+++ precache-manifest.456b3447b60d08d47cbdd58d7b984732.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f2a40a19cacbebf46687867c4f62ef",
+    "url": "/port-two/index.html"
+  },
+  {
+    "revision": "a1763796f56dd5c67e67",
+    "url": "/port-two/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "c96b7e4d6286c60ec7de",
+    "url": "/port-two/static/css/main.f71de65b.chunk.css"
+  },
+  {
+    "revision": "a1763796f56dd5c67e67",
+    "url": "/port-two/static/js/2.934937d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/port-two/static/js/2.934937d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96b7e4d6286c60ec7de",
+    "url": "/port-two/static/js/main.cb93f561.chunk.js"
+  },
+  {
+    "revision": "103b9590865ad61ca32c",
+    "url": "/port-two/static/js/runtime-main.a85c88a6.js"
+  },
+  {
+    "revision": "98d38f228ff83decd93d491c12414a22",
+    "url": "/port-two/static/media/Cloud-20s-1084px.98d38f22.svg"
+  },
+  {
+    "revision": "f979460b85668582f989a41a89657ae1",
+    "url": "/port-two/static/media/Flat-Mountains.f979460b.svg"
+  },
+  {
+    "revision": "681f167f9f8594757ff6734ce1002e6b",
+    "url": "/port-two/static/media/close.681f167f.svg"
+  },
+  {
+    "revision": "676a90226d206d911f9468b1c79db785",
+    "url": "/port-two/static/media/downarrow.676a9022.svg"
+  },
+  {
+    "revision": "3c19c05e1c2306015a0fc84bf77c6010",
+    "url": "/port-two/static/media/drawer1.3c19c05e.svg"
+  },
+  {
+    "revision": "772d0e3dd5d9b7042258fd3220204e37",
+    "url": "/port-two/static/media/pinkcloud.772d0e3d.svg"
+  },
+  {
+    "revision": "6c5dffc52ef77b235e3447fabe546d80",
+    "url": "/port-two/static/media/profile1.6c5dffc5.svg"
+  }
+]);
